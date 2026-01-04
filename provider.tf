dd6d7fd01f6dfeef041fd2a0bd6cd157dd0d5bd5
sowmya <sowmyaracherla3@gmail.com>
2026-01-04 17:16:18 +0530

    workstation-cluster

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.0.0", # Use the latest beta version for testing
+    }
+  }
+
+  backend "s3" {
+    bucket         = "roboshop-k8s"
+    key            = "eksctl"
+    region         = "us-east-1"
+    # dynamodb_table = "Roboshop-dev-tfstate-locks"
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}   
